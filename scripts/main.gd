9f65c64f65c544ae76e49eb7337b3cb9d8fb270c
lakshraja <lakshraja08@gmail.com>
2025-08-26 18:23:29 +0530

    basic player movement and structure of game

---
+++ scripts/main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+func _ready() -> void:
+	pass
+
+#this guy will load the menu first
+#then it will manage all the scenes and levels and also game over window
+#it should also manage the ui like the cookie counter
+
+func _process(delta: float) -> void:
+	pass
